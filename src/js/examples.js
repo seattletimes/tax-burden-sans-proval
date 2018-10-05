48a381ac059690211cb06c07bf50d1b0768f2227
sfmina <fmina@seattletimes.com>
2018-10-04 17:51:59 -0700

    Text updates

--- src/js/examples.js
+++ src/js/examples.js
@@ -4,7 +4,7 @@ module.exports = {
       bizIncome: 0, //20727000,
       employees: 0, //124,
       //space: 41300 * 96, // supermarket avg size * avg of the 10 supermarket value/sqft from spreadsheet
-      taxableSales: 0, //0.005,
+      taxableSales: 0.005,
       bizType: "retail",
     },
   },
@@ -13,7 +13,7 @@ module.exports = {
       bizIncome: 0, // 22483500,
       employees: 0, // 117,
       //space: 23400 * 248, // See note above
-      taxableSales: 0, //0.005,
+      taxableSales: 0.005,
       bizType: "services",
     },
   },
